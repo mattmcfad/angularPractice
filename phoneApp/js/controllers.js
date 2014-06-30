0a079ee64865dde79477966d8350cdc7af868dff
Matt McFadyen <mmcfadyen91@gmail.com>
2014-06-30 17:19:09 -0400

    step02 on angularjs docs

---
+++ phoneApp/js/controllers.js
@@ -0,0 +1,33 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
+
+
+phonecatApp.controller('WebDevCtrl', function ($scope) {
+  $scope.technology = [
+    {'name': 'Grunt',
+     'company': 'Google',
+ 	 'description':'Task runner'},
+    {'name': 'Bower',
+     'company': 'Twitter',
+ 	 'description':'Dependency generator'},
+ 	{'name': 'Yeoman',
+     'company': 'Google',
+ 	 'description':'Scaffolding tool'},
+ 	{'name': 'Angular',
+     'company': 'Google',
+ 	 'description':'MVC Framework'},
+ 	{'name': 'Gulp',
+     'company': 'Fractal',
+ 	 'description':'Task runner'}
+  ];
+});
