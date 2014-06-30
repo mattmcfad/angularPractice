9f57ba65d2a359405fa58109507b743ef9ace3d8
Matt McFadyen <mmcfadyen91@gmail.com>
2014-06-30 18:23:57 -0400

    step4 twoway data binding

--- phoneApp/js/controllers.js
+++ phoneApp/js/controllers.js
@@ -1,33 +1,34 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var webdevApp = angular.module('webdevApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-});
-
-
-phonecatApp.controller('WebDevCtrl', function($scope) {
+webdevApp.controller('WebDevCtrl', function($scope) {
   $scope.technology = [
     {'name': 'Grunt',
      'company': 'Google',
- 	 'description':'Task runner'},
+ 	 'description':'Task runner',
+ 	 'age': 2011,
+ 	},
     {'name': 'Bower',
      'company': 'Twitter',
- 	 'description':'Dependency generator'},
+ 	 'description':'Dependency generator',
+ 	 'age': 2013
+ 	},
  	{'name': 'Yeoman',
      'company': 'Google',
- 	 'description':'Scaffolding tool'},
+ 	 'description':'Scaffolding tool',
+ 	 'age': 2013
+ 	},
  	{'name': 'Angular',
      'company': 'Google',
- 	 'description':'MVC Framework'},
+ 	 'description':'MVC Framework',
+ 	 'age': 2012
+ 	},
  	{'name': 'Gulp',
      'company': 'Fractal',
- 	 'description':'Task runner'}
+ 	 'description':'Task runner',
+ 	 'age': 2014
+ 	}
   ];
+
+  //Default order by
+  $scope.orderProp = 'age';
 });
