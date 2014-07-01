2cf6fb5f9f9c1c39fd447f6aa5174dc46db4e02a
Matt McFadyen <mmcfadyen91@gmail.com>
2014-06-30 20:42:21 -0400

    step05 http request

--- phoneApp/js/controllers.js
+++ phoneApp/js/controllers.js
@@ -1,34 +1,45 @@
 var webdevApp = angular.module('webdevApp', []);
 
-webdevApp.controller('WebDevCtrl', function($scope) {
-  $scope.technology = [
-    {'name': 'Grunt',
-     'company': 'Google',
- 	 'description':'Task runner',
- 	 'age': 2011,
- 	},
-    {'name': 'Bower',
-     'company': 'Twitter',
- 	 'description':'Dependency generator',
- 	 'age': 2013
- 	},
- 	{'name': 'Yeoman',
-     'company': 'Google',
- 	 'description':'Scaffolding tool',
- 	 'age': 2013
- 	},
- 	{'name': 'Angular',
-     'company': 'Google',
- 	 'description':'MVC Framework',
- 	 'age': 2012
- 	},
- 	{'name': 'Gulp',
-     'company': 'Fractal',
- 	 'description':'Task runner',
- 	 'age': 2014
- 	}
-  ];
 
-  //Default order by
+webdevApp.controller('WebDevCtrl', function ($scope, $http) {
+  $http.get('webdev.json').success(function(data) {
+    $scope.technology = data;
+  });
+
   $scope.orderProp = 'age';
 });
+
+
+
+// webdevApp.controller('WebDevCtrl', function($scope) {
+//   $scope.technology = [
+//     {'name': 'Grunt',
+//      'company': 'Google',
+//  	 'description':'Task runner',
+//  	 'age': 2011,
+//  	},
+//     {'name': 'Bower',
+//      'company': 'Twitter',
+//  	 'description':'Dependency generator',
+//  	 'age': 2013
+//  	},
+//  	{'name': 'Yeoman',
+//      'company': 'Google',
+//  	 'description':'Scaffolding tool',
+//  	 'age': 2013
+//  	},
+//  	{'name': 'Angular',
+//      'company': 'Google',
+//  	 'description':'MVC Framework',
+//  	 'age': 2012
+//  	},
+//  	{'name': 'Gulp',
+//      'company': 'Fractal',
+//  	 'description':'Task runner',
+//  	 'age': 2014
+//  	}
+//   ];
+
+//   //Default order by
+//   $scope.orderProp = 'age';
+// });
